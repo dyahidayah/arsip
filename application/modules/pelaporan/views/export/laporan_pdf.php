8856ad1c180968c355ba15b49a276bab3d55dce0
unknown <dyahidayah03@gmail.com>
2024-07-30 00:05:33 +0700

    first

--- application/modules/pelaporan/views/export/laporan_pdf.php
+++ application/modules/pelaporan/views/export/laporan_pdf.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List Machine PT Parkland World Indonesia Jepara</title>
+    <title>LAPORAN SURAT MASUK DAN KELUAR SATHARMATTIM</title>
     <style>
         table {
             width: 100%;
